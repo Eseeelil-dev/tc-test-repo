e14136b69cc8c48ac2ae5928a1c450d1b401b45d
Eseeelil-dev <eseeelil@gmail.com>
2020-08-27 22:13:30 +0300

    Next 1

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 'use strict';
 
-console.log(`##teamcity[blockOpened name='Start' description='Starting test run' flowId='1']`);
-console.log(`Start tests`);
-console.log(`User agent chrome`);
-console.log(`Test count 10`);
-console.log(`##teamcity[blockClosed name='Start' flowId='1']`);
+function logBlock() {
+    console.log(`##teamcity[blockOpened name='Start' description='Starting test run' flowId='1']`);
+    console.log(`Start tests`);
+    console.log(`User agent chrome`);
+    console.log(`Test count 10`);
+    console.log(`##teamcity[blockClosed name='Start' flowId='1']`);
+}
 
-console.log(`##teamcity[testSuiteStarted name='Permit' flowId='1']`);
+function testSuiteStarted(name, flowId) {
+    console.log(`##teamcity[testSuiteStarted name='${name}' flowId='${flowId}']`);
+}
 
-console.log(`##teamcity[testStarted name='Create a permit' flowId='1']`);
+function testSuiteFinished(name, flowId) {
+    console.log(`##teamcity[testSuiteFinished name='${name}' flowId='${flowId}']`);
+}
 
-console.log(`Open url: http://google.com`);
-console.log(`Type text: blah blah`);
-console.log(`Click search button`);
-console.log(`Check blah blah`);
+function testStarted(name, flowId) {
+    console.log(`##teamcity[testStarted name='${name}' flowId='${flowId}']`);
+}
 
-console.log(`##teamcity[testFinished name='Create a permit' duration='12500' flowId='1']`);
+function testFinished(name, duration, flowId) {
+    console.log(`##teamcity[testFinished name='${name}' duration='${duration}' flowId='${flowId}']`);
+}
 
-console.log(`##teamcity[testSuiteFinished name='Permit' flowId='1']`);
+function testMessage(name, message) {
+    console.log(`##teamcity[message text='${message}']`);
+}
+
+logBlock();
+testSuiteStarted('Permit', '1');
+testStarted('Create a permit', '1');
+testMessage('Create a permit', `Open url: http://google.com`);
+testMessage('Create a permit', `Type text: blah blah`);
+testMessage('Create a permit', `Click search button`);
+testMessage('Create a permit', `Check blah blah`);
+testFinished('Create a permit', '12050', 1);
+testSuiteFinished('Permit', '1');
