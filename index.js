5195681add81e20da865c84a0c0a221c9ed290a5
Eseeelil-dev <eseeelil@gmail.com>
2020-10-03 11:58:31 +0300

    Recreacte logger

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ function logBlock() {
     console.log(`##teamcity[blockClosed name='Start (block)' flowId='0']`);
 }
 
-function testStarted(name, flowId) {
-    console.log(`##teamcity[testStarted name='${name} (Test)' flowId='${flowId}']`);
-}
-
-function testFinished(name, duration, flowId) {
-    console.log(`##teamcity[testFinished name='${name} (Test)' duration='${duration}' flowId='${flowId}']`);
-}
-
 function testMessage(name, message, flowId) {
     console.log(`##teamcity[message text='${name}: ${message} (Message)' flowId='${flowId}']`);
 }
@@ -31,26 +23,22 @@ const ids = [ 1, 2 ];
 
 logBlock();
 
-test('Create a permit', async () => {
-    testStarted(tests[0], ids[0]);
-    testMessage(tests[0], `Open url: http://google.com`, ids[0]);
+test('Create a permit', async (logStream) => {
+    logStream.testMessage(`Open url: http://google.com`);
     await delay(205);
-    testMessage(tests[0], `Type text: blah blah`, ids[0]);
-    testMessage(tests[0], `Click search button`, ids[0]);
+    logStream.testMessage(`Type text: blah blah`);
+    logStream.testMessage(`Click search button`);
     await delay(10);
-    testMessage(tests[0], `Check blah blah`, ids[0]);
-    testFinished(tests[0], '12050', ids[0]);
+    logStream.testMessage(`Check blah blah`);
 });
 
-test('Create a isolation', async () => {
-    testStarted(tests[1], ids[1]);
-    testMessage(tests[1], `Open url: http://google.com/isolation`, ids[1]);
+test('Create a isolation', async (logStream) => {
+    logStream.testMessage(`Open url: http://google.com/isolation`);
     await delay(200);
-    testMessage(tests[1], `Type text: blah blah isolation`, ids[1]);
+    logStream.testMessage(`Type text: blah blah isolation`);
     await delay(13);
-    testMessage(tests[1], `Click search button isolation`, ids[1]);
-    testMessage(tests[1], `Check blah blah isolation`, ids[1]);
-    testFinished(tests[1], '11100', ids[1]);
+    logStream.testMessage(`Click search button isolation`);
+    logStream.testMessage(`Check blah blah isolation`);
 });
 
 run();
--- runner.js
+++ runner.js
@@ -2,14 +2,45 @@
 
 const tests = [];
 
+class LogStream {
+    constructor(name) {
+        this.name = name;
+        this.messages = [];
+    }
+
+    testStarted() {
+        this.messages.push(`##teamcity[testStarted name='${this.name} (Test)' flowId='${this.name}']`);
+    }
+
+    testFinished(duration) {
+        this.messages.push(`##teamcity[testFinished name='${this.name} (Test)' duration='${duration}' flowId='${this.name}']`);
+    }
+
+    testMessage(message) {
+        this.messages.push(`##teamcity[message text='${this.name}: ${message} (Message)' flowId='${this.name}']`);
+    }
+
+    flush() {
+        for (const msg of this.messages) {
+            console.log(msg);
+        }
+    }
+}
+
 function test(name, fn) {
     const fn1 = async () => {
+        let logStream = new LogStream(name);
+        let passed = false;
+        let startedTime = Date.now();
         try {
-            await fn();
-            return [name, true];
+            logStream.testStarted();
+            await fn(logStream);
+            passed = true;
         } catch (e) {
-            return [name, false];
         }
+        logStream.testFinished(Date.now() - startedTime);
+        logStream.flush();
+        return [name, passed];
     };
     tests.push({name, func: fn1});
 }
@@ -19,8 +50,8 @@ function run() {
         // Start all tests
         .all(tests.map(({func}) => func()))
         // Get the results
-        .then(results => {
-            process.exit(results.filter(res => res[1]).length);
+        .then(_ => {
+            process.exit(0);
         });
 }
 
